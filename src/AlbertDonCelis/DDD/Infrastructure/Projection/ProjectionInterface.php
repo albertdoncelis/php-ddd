9ceb33fe21c82b969a26a186cb581cb4eb0b4e95
albert <albert@codeninja.co>
2019-04-11 09:25:09 +0800

    add projection

---
+++ src/AlbertDonCelis/DDD/Infrastructure/Projection/ProjectionInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: albert
+ * Date: 2019-04-11
+ * Time: 08:04
+ */
+
+namespace AlbertDonCelis\DDD\Infrastructure\Projection;
+
+use AlbertDonCelis\DDD\Domain\EventInterface;
+
+interface ProjectionInterface
+{
+
+    public function listenToEventNameOf(): string;
+
+    public function project(EventInterface $event): void;
+}
---
+++ src/AlbertDonCelis/DDD/Infrastructure/Projector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AlbertDonCelis\DDD\Infrastructure;
+
+use AlbertDonCelis\DDD\Domain\EventInterface;
+use AlbertDonCelis\DDD\Infrastructure\Projection\ProjectionInterface;
+
+class Projector
+{
+    private $projections = [];
+
+    public function register(ProjectionInterface $projection): self
+    {
+        $this->projections[$projection->listenToEventNameOf()][] = $projection;
+
+        return $this;
+    }
+
+    public function registeredProjections(): array
+    {
+        return $this->projections;
+    }
+
+    private function hasSubscriber(string $listenToEventNameOf): bool
+    {
+        return array_key_exists($listenToEventNameOf, $this->projections);
+    }
+
+    public function subscribeTo(EventInterface $event)
+    {
+        $eventListenTo = $event->eventName();
+
+        if ($this->hasSubscriber($eventListenTo)) {
+
+            /** @var ProjectionInterface $projection */
+            foreach ($this->projections[$eventListenTo] as $projection) {
+                $projection->project($event);
+            }
+        }
+    }
+}
